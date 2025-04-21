4d1e67d96a59901c3f26cae4c1163f95b581e641
loli667 <hasennazerke667@gmail.com>
2025-04-21 13:12:53 +0500

    калькулятор

--- client/src/routes/Routes.jsx
+++ client/src/routes/Routes.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from '../Pages/Home';
 import Authentication from '../Pages/Authentication';
 import Registration from '../Pages/Registration'; // Импорт страницы регистрации
+import CalorieCalculator from '../Pages/CalorieCalculator'; // Импорт страницы "Калория калькулятор"
 
 const AppRoutes = () => {
   return (
@@ -11,6 +12,7 @@ const AppRoutes = () => {
         <Route path="/" element={<Home />} />
         <Route path="/auth" element={<Authentication />} />
         <Route path="/register" element={<Registration />} /> {/* Маршрут для регистрации */}
+        <Route path="/calorie-calculator" element={<CalorieCalculator />} /> {/* Маршрут для калькулятора */}
       </Routes>
     </Router>
   );
